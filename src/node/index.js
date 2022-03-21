f871c23cf78dcd978dada00adad4dc1a19210496
Samuel Santos <samuel@propper.com.au>
2022-03-22 01:05:17 +1100

    Added project files.

---
+++ src/node/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser'); 
+const config ={
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'appdb'
+};
+
+app.use(bodyParser.json()); 
+const mysql = require('mysql');
+
+app.get('/', (req, res)=>{
+
+    const connection = mysql.createConnection(config);
+    const sql = `SELECT name FROM people`
+
+    let htmlOutput = '<h1>Full cycle Rocks!</h1></br>';
+    htmlOutput += '<ul>';
+    connection.query(sql, (err, rows)=>{
+        rows.forEach(row => {
+            htmlOutput += `<li>${row.name}</li>`;
+        });
+        
+        htmlOutput += '</ul>';
+        res.send(htmlOutput);
+    });
+    connection.end();
+});
+
+app.get('/people', (req, res)=>{
+    const connection = mysql.createConnection(config);
+    const sql = `SELECT name FROM people`
+
+    let htmlOutput = '<ul>';
+    connection.query(sql, (err, rows)=>{
+        rows.forEach(row => {
+            htmlOutput += `<li>${row.name}</li>`;
+        });
+        
+        htmlOutput += '</ul>';
+        res.send(htmlOutput);
+    });
+    connection.end();
+});
+
+app.post('/people', (req, res)=>{
+    let username=req.body.name;
+    const connection = mysql.createConnection(config);
+    const sql = `INSERT INTO people(name) VALUES ('${username}')`;
+    connection.query(sql, (err)=>{
+        if(err){
+            res.send(err);
+        }
+        else{
+            res.send('<h2>Usuario cadastrado com sucesso!</h2>');
+        }
+    });
+    connection.end();
+});
+
+app.listen(port, ()=>{
+    console.log(`App runing on port ${port}.`)
+});
